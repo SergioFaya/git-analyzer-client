ee698b1117af780f82ffc7f00ae86df382045adf
SergioFaya <estisergio@gmail.com>
2019-03-16 01:14:53 +0100

    release arreglando las urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: 'ec2-34-243-245-144.eu-west-1.compute.amazonaws.com',
-  serverUrl: 'ec2-18-203-224-254.eu-west-1.compute.amazonaws.com',
+  authUrl: 'http://ec2-34-243-245-144.eu-west-1.compute.amazonaws.com',
+  serverUrl: 'http://ec2-18-203-224-254.eu-west-1.compute.amazonaws.com',
 };
