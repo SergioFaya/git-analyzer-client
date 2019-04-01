a09429d2dc2648f6ac43af6dc642ba0ffec1a3e8
SergioFaya <estisergio@gmail.com>
2019-04-01 21:33:51 +0200

    format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
 
 	/**
 	 * Prepares the socket for receiving info based on the state event
-	 * @param state 
+	 * @param state
 	 */
 	public initSocket(state: string): void {
 		this.socket = socketIo('http://localhost:3000');
@@ -51,6 +51,11 @@ export class AppComponent implements OnInit {
 	private login(token, githubToken) {
 		this.storeTokens(token, githubToken)
 		this.dataService.loggedUser(true);
+		this.authService.getUserInfo()
+		.then(
+			x => console.log(x)
+		)
+		.catch(err => console.log(err));
 	}
 
 	get logoutFunc() {
--- src/app/services/AuthService/auth.service.ts
+++ src/app/services/AuthService/auth.service.ts
@@ -4,66 +4,68 @@ import { environment } from 'src/environments/environment';
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
 
-  readonly AUTH_SERVICE_URL = environment.authUrl;
-  readonly SERVER_SERVICE_URL = environment.serverUrl;
+	readonly AUTH_SERVICE_URL = environment.authUrl;
+	readonly SERVER_SERVICE_URL = environment.serverUrl;
 
-  readonly LOGOUT_ADDRESS: string = '/logout';
-  readonly LOGIN_ADDRESS: string = '/login';
-  readonly USER_INFO: string = '/user/info';
+	readonly LOGOUT_ADDRESS: string = '/logout';
+	readonly LOGIN_ADDRESS: string = '/login';
+	readonly USER_INFO: string = '/user/info';
 
-  private githubToken;
-  private serverToken;
+	private githubToken;
+	private serverToken;
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  setGithubToken(githubToken: string) {
-    this.githubToken = githubToken;
-  }
+	setGithubToken(githubToken: string) {
+		this.githubToken = githubToken;
+	}
 
-  setServerToken(serverToken: string) {
-    this.serverToken = serverToken;
-  }
+	setServerToken(serverToken: string) {
+		this.serverToken = serverToken;
+	}
 
-  getGithubToken(): string {
-    return this.githubToken;
-  }
+	getGithubToken(): string {
+		return this.githubToken;
+	}
 
-  getServerToken(): string {
-    return this.serverToken;
-  }
+	getServerToken(): string {
+		return this.serverToken;
+	}
 
-  logOut() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      })
-    }
-    return this.http.post(this.AUTH_SERVICE_URL + this.LOGOUT_ADDRESS,{'x-access-token': localStorage.getItem('accessToken')}, httpOptions).toPromise();
-  }
-  getLoginUrl() {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
-    // TODO: meter las rutas de la api en un fichero de configuración
-    return this.http.get(this.AUTH_SERVICE_URL + this.LOGIN_ADDRESS, httpOptions).toPromise();
-  }
+	logOut() {
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+			})
+		}
+		const body = { 'x-access-token': localStorage.getItem('accessToken') };
 
-  // TODO: llamarla para pillar la foto y la info básica
-  getUserInfo() {
-    const headers = {
-      'Content-Type': 'application/json',
-      'x-access-token': this.serverToken,
-      'x-github-token': this.githubToken
-    };
-    const httpOptions = {
-      headers: new HttpHeaders(headers)
-    };
-    return this.http.get(this.SERVER_SERVICE_URL + this.USER_INFO, httpOptions);
-  }
+		return this.http.post(this.AUTH_SERVICE_URL + this.LOGOUT_ADDRESS, body, httpOptions).toPromise();
+	}
+	getLoginUrl() {
+		const httpOptions = {
+			headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+		};
+		// TODO: meter las rutas de la api en un fichero de configuración
+		return this.http.get(this.AUTH_SERVICE_URL + this.LOGIN_ADDRESS, httpOptions).toPromise();
+	}
+
+	// TODO: llamarla para pillar la foto y la info básica
+	getUserInfo() {
+		const headers = {
+			'Content-Type': 'application/json',
+			'x-access-token': this.serverToken,
+			'x-github-token': this.githubToken
+		};
+		const httpOptions = {
+			headers: new HttpHeaders(headers)
+		};
+		return this.http.get(this.SERVER_SERVICE_URL + this.USER_INFO, httpOptions).toPromise();
+	}
 
 
 }
