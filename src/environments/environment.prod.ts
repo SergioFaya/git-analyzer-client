5f35dd4b7e4d30964b5167de9f9e6338a92010f6
SergioFaya <estisergio@gmail.com>
2019-06-27 20:43:05 +0200

    bugfixing server address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authUrl: '156.35.163.128:/3000',
-  serverUrl: '156.35.163.128:/3001',
+  authUrl: 'http://156.35.163.128:3000',
+  serverUrl: 'http://156.35.163.128:3001',
   client_id: 'c4c42af4e127583d6c40'
 
 };
